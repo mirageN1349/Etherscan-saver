310268adb0956205388a6aa7a739f8debe5f4bfa
MirageN1349 <desertirpipi@gmail.com>
2022-05-24 13:28:42 +0300

    update migrations

--- src/migration/1653388037327-init-db.ts
+++ src/migration/1653388037327-init-db.ts
@@ -1,16 +1,20 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class initDb1653384715335 implements MigrationInterface {
-    name = 'initDb1653384715335'
+export class initDb1653388037327 implements MigrationInterface {
+    name = 'initDb1653388037327'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "app_entity" ("id" SERIAL NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, CONSTRAINT "PK_785e980b5ae9d53bf5e6dc74c85" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "transactions" ("id" SERIAL NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, "blockNumber" bytea NOT NULL, "from" character varying NOT NULL, "to" character varying, "value" numeric NOT NULL, "blockId" integer, CONSTRAINT "PK_a219afd8dd77ed80f5a862f1db9" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "blocks" ("id" SERIAL NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, "number" character varying NOT NULL, CONSTRAINT "PK_8244fa1495c4e9222a01059244b" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "transactions" ADD CONSTRAINT "FK_e11180855c1afd8fe21f96a1bf8" FOREIGN KEY ("blockId") REFERENCES "blocks"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "transactions" DROP CONSTRAINT "FK_e11180855c1afd8fe21f96a1bf8"`);
+        await queryRunner.query(`DROP TABLE "blocks"`);
         await queryRunner.query(`DROP TABLE "transactions"`);
+        await queryRunner.query(`DROP TABLE "app_entity"`);
     }
 
 }
